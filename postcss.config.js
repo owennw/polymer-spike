640d9a5b666a46b6ead0bbdb1a958c212787ec7b
Nick Owen <nowen@scottlogic.co.uk>
2017-11-24 15:33:55 +0000

    delete postcss config

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  parser: 'sugarss',
-  map: false,
-  plugins: [
-    require('postcss-smart-import')({ /* ...options */ }),
-    require('precss')({ /* ...options */ }),
-    require('autoprefixer')({ /* ...options */ })
-  ]
-}
